24a080c453e60ed805672c40b119b7c5341e63a1
Lucas Dominguez <lucasfdominguez4178@gmail.com>
2025-10-01 10:56:15 -0300

    codigo atualizado, com projeto finalizado, adicionei uma linha de codigo para aceitar altura com virgula ou ponto

--- amostragem.py
+++ amostragem.py
@@ -1,2 +1,6 @@
 nome = input("Digite seu nome: ")
-print(f"Olá, {nome}!")
+idade = int(input("Digite sua Idade: "))
+altura_str = (input("Digite sua altura: "))
+altura = float(altura_str.replace(",","."))
+
+print(f"Olá, %s, voce tem %d e mede %.2f metros!" %(nome, idade, altura) ) 
